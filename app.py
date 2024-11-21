2d84ec66c49d4a8c31914a88fe6d9f76a3430b38
IlProfColazzo <72198473+IlProfColazzo@users.noreply.github.com>
2024-11-21 17:23:32 +0100

    Soluzione

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/francesco")
+def hello_francesco():
+    str = "Ciao a tutti"
+    return "<p>"+str+"</p>"
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+#creare una welcome page, una about us page e una contact page.
+#Tre route differenti
+#All'interno delle tre pagine inserite una navbar (con i tre link.
+#Attenzione i link non devono portare da nessuna parte
+
+app.run(debug=True)
